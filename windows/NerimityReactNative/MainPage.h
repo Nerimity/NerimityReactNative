b7fe1b411226555fca720f71fdb60dbddf934b70
Supertiger <saawalkhn@googlemail.com>
2023-06-03 09:46:01 +0100

    added windows support

---
+++ windows/NerimityReactNative/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::NerimityReactNative::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::NerimityReactNative::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
