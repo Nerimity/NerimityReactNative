0357d14ad92fd503bef3f1552a867f6e22d001ae
Supertiger <saawalkhn@googlemail.com>
2023-06-17 09:13:13 +0100

    feat: :sparkles: Added typing indicator

--- src/components/MessagesView.tsx
+++ src/components/MessagesView.tsx
@@ -4,9 +4,22 @@ import {
   useNavigation,
   useRoute,
 } from '@react-navigation/native';
-import React, {startTransition, useCallback, useEffect, useState} from 'react';
+import React, {
+  startTransition,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 
-import {View, StyleSheet, TextInput, StatusBar, AppState} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  StatusBar,
+  AppState,
+  Text,
+} from 'react-native';
 import App, {RootStackParamList} from '../../App';
 import {useStore} from '../store/store';
 import {observer} from 'mobx-react-lite';
@@ -17,6 +30,8 @@ import Header from './ui/Header';
 import Colors from './ui/Colors';
 import {FlashList} from '@shopify/flash-list';
 import {ChannelDetailsScreenNavigationProp} from './ChannelDetailsView';
+import {RawMessage} from '../store/RawData';
+import {ServerEvents} from '../store/EventNames';
 
 export type MainScreenRouteProp = RouteProp<RootStackParamList, 'Message'>;
 export type MainScreenNavigationProp = NavigationProp<RootStackParamList>;
@@ -69,6 +84,7 @@ const MessageList = observer(() => {
     <FlashList
       data={(messages || []).slice()}
       estimatedItemSize={53}
+      contentContainerStyle={{paddingTop: 20}}
       inverted
       showsVerticalScrollIndicator={false}
       keyExtractor={item => item.id}
@@ -88,9 +104,129 @@ const MessageList = observer(() => {
 const InputArea = () => {
   return (
     <View>
+      <TypingIndicator />
       <CustomInput />
     </View>
   );
+};
+
+interface TypingPayload {
+  userId: string;
+  channelId: string;
+}
+
+const TypingIndicator = () => {
+  const route = useRoute<MainScreenRouteProp>();
+  const {socket, users} = useStore();
+  const [typingUserIds, setTypingUserIds] = useState<
+    Record<string, number | undefined>
+  >({});
+
+  const onTyping = useCallback(
+    (event: TypingPayload) => {
+      if (event.channelId !== route.params.channelId) {
+        return;
+      }
+      if (typingUserIds[event.userId]) {
+        clearTimeout(typingUserIds[event.userId]!);
+      }
+      const timeoutId = setTimeout(
+        () =>
+          setTypingUserIds(current => {
+            const copy = {...current};
+            delete copy[event.userId];
+            return copy;
+          }),
+        5000,
+      );
+      setTypingUserIds({...typingUserIds, [event.userId]: timeoutId});
+    },
+    [route.params.channelId, typingUserIds],
+  );
+
+  const onMessageCreated = useCallback(
+    (event: {message: RawMessage}) => {
+      if (event.message.channelId !== route.params.channelId) {
+        return;
+      }
+      const timeoutId = typingUserIds[event.message.createdBy.id];
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+        setTypingUserIds(current => {
+          const copy = {...current};
+          delete copy[event.message.createdBy.id];
+          return copy;
+        });
+      }
+    },
+    [route.params.channelId, typingUserIds],
+  );
+  const onMessageUpdated = useCallback(
+    (evt: any) => onMessageCreated({message: evt.updated}),
+    [onMessageCreated],
+  );
+
+  useEffect(() => {
+    socket.io.on(ServerEvents.CHANNEL_TYPING, onTyping);
+    socket.io.on(ServerEvents.MESSAGE_CREATED, onMessageCreated);
+    socket.io.on(ServerEvents.MESSAGE_UPDATED, onMessageUpdated);
+
+    return () => {
+      socket.io.off(ServerEvents.CHANNEL_TYPING, onTyping);
+      socket.io.off(ServerEvents.MESSAGE_CREATED, onMessageCreated);
+      socket.io.off(ServerEvents.MESSAGE_UPDATED, onMessageUpdated);
+    };
+  }, [onMessageCreated, onMessageUpdated, onTyping, socket.io]);
+
+  const typingUsers = useMemo(
+    () => Object.keys(typingUserIds).map(userId => users.get(userId)),
+    [typingUserIds, users],
+  );
+
+  if (!typingUsers.length) {
+    return null;
+  }
+
+  return (
+    <Floating>
+      <Text style={styles.typingIndicatorText} numberOfLines={1}>
+        {typingUsers.length === 1 && (
+          <Text>
+            <B>{typingUsers[0]?.username}</B> is typing...
+          </Text>
+        )}
+        {typingUsers.length === 2 && (
+          <Text>
+            <B>{typingUsers[0]?.username}</B> and{' '}
+            <B>{typingUsers[0]?.username}</B> are typing...
+          </Text>
+        )}
+        {typingUsers.length === 3 && (
+          <Text>
+            <B>{typingUsers[0]?.username}</B>, <B>{typingUsers[1]?.username}</B>{' '}
+            and <B>{typingUsers[2]?.username}</B> are typing...
+          </Text>
+        )}
+        {typingUsers.length > 3 && (
+          <Text>
+            <B>{typingUsers[0]?.username}</B>, <B>{typingUsers[1]?.username}</B>{' '}
+            and <B>{typingUsers[2]?.username}</B> and{' '}
+            <B>{(typingUsers.length - 3).toString()}</B> others are typing...
+          </Text>
+        )}
+      </Text>
+    </Floating>
+  );
+};
+
+const B = (props: {children?: string}) => (
+  <Text style={styles.b} children={props.children} />
+);
+
+const Floating = (props: {
+  children: React.JSX.Element | React.JSX.Element[];
+}) => {
+  return <View style={styles.floating} children={props.children} />;
 };
 
 const CustomInput = () => {
@@ -156,6 +292,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.6)',
     borderRadius: 8,
     margin: 5,
+    marginTop: 0,
     borderStyle: 'solid',
     borderWidth: 1,
     borderColor: 'rgba(255,255,255,0.2)',
@@ -170,5 +307,23 @@ const styles = StyleSheet.create({
   sendButton: {
     paddingLeft: 20,
     paddingRight: 20,
+  },
+  b: {fontWeight: 'bold'},
+  floating: {
+    position: 'absolute',
+    zIndex: 1111,
+    backgroundColor: Colors.paneColor,
+    padding: 3,
+    paddingLeft: 8,
+    paddingRight: 8,
+    borderRadius: 12,
+    borderColor: 'rgba(255, 255, 255, 0.2)',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    top: -10,
+    left: 10,
+  },
+  typingIndicatorText: {
+    fontSize: 10,
   },
 });
